47098577a7ab6d21aaac420a82b5f7a494516eca
itoakihiro555 <tdnnekozuki@gmail.com>
2026-05-19 16:02:48 +0900

    POSTエンドポイントとAPI仕様を追加

--- myapp/app.js
+++ myapp/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-
+app.use(express.json());
+app.use(express.static('public'));
 // ルート1：トップページ
 app.get('/', (req, res) => {
   res.send('トップページです');
@@ -17,6 +18,27 @@ app.get('/time', (req, res) => {
   res.send('現在時刻：' + now);
 });
 
+app.get("/api/test", (req, res) => {
+  res.json({ message: "APIが動いています", status: "ok" });
+});
+
+app.get('/status', (req, res) => {
+  res.json({ status: 'ok', message: 'サーバーが動いています' });
+});
+const messages = [];
+
+// GET：メッセージ一覧を取得
+app.get("/api/messages", (req, res) => {
+  res.json(messages);
+});
+
+app.post("/api/messages", (req, res) => {
+  const { username, text } = req.body;
+  const newMessage = { id: messages.length + 1, username, text };
+  messages.push(newMessage);
+  res.json(newMessage);
+  });
+  
 app.listen(3000, () => {
   console.log('サーバーが起動しました: http://localhost:3000');
 });
