9eb62de202aac4bddd3181e5323a0a86ebfc5d17
itoakihiro555 <tdnnekozuki@gmail.com>
2026-06-01 22:49:29 +0900

    ポート番号を環境変数から読むように変更

--- myapp/app.js
+++ myapp/app.js
@@ -1,7 +1,19 @@
+require('dotenv').config();
 const express = require('express');
+const{Pool} = require('pg');
+
 const app = express();
 app.use(express.json());
 app.use(express.static('public'));
+
+const pool = new Pool({
+  host:     process.env.DB_HOST,
+  port:     process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
 // ルート1：トップページ
 app.get('/', (req, res) => {
   res.send('トップページです');
@@ -28,17 +40,38 @@ app.get('/status', (req, res) => {
 const messages = [];
 
 // GET：メッセージ一覧を取得
-app.get("/api/messages", (req, res) => {
-  res.json(messages);
+//app.get("/api/messages", (req, res) => {
+  //res.json(messages);
+//});
+
+app.get('/api/messages', async (req, res) => {
+  const result = await pool.query(
+    'SELECT * FROM messages ORDER BY created_at ASC'
+  );
+  res.json(result.rows);
 });
 
-app.post("/api/messages", (req, res) => {
-  const { username, text } = req.body;
-  const newMessage = { id: messages.length + 1, username, text };
-  messages.push(newMessage);
-  res.json(newMessage);
-  });
+//app.post("/api/messages", (req, res) => {
+  //const { username, text } = req.body;
+  //const newMessage = { id: messages.length + 1, username, text };
+  //messages.push(newMessage);
+  //res.json(newMessage);
+  //});
   
-app.listen(3000, () => {
-  console.log('サーバーが起動しました: http://localhost:3000');
+app.post('/api/messages', async (req, res) => {
+  const { username, text } = req.body;
+  const result = await pool.query(
+    'INSERT INTO messages (username, text) VALUES ($1, $2) RETURNING *',
+    [username, text]
+  );
+  res.json(result.rows[0]);
+});
+
+//app.listen(3000, () => {
+  //console.log('サーバーが起動しました: http://localhost:3000');
+//});
+
+// 変更後
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`サーバが起動しました : http://localhost:${process.env.PORT || 3000}`);
 });
